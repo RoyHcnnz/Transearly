60083c2d76dac677f1817fbb16153c04e7831b8a
RoyHcnnz <rhuangcnnz@gmail.com>
2023-07-08 16:31:37 -0700

    set up translator

---
+++ TranslateCore.py
@@ -0,0 +1,49 @@
+import http.client
+import Secrets
+import json
+
+class Translator:
+
+    conn = http.client.HTTPSConnection(Secrets.TRANLATOR_HOST)
+    headers = {
+        'content-type': "application/json",
+        'X-RapidAPI-Key': Secrets.TRANLATOR_API_KEY,
+        'X-RapidAPI-Host': Secrets.TRANLATOR_HOST
+    }
+
+    def __init__(self):
+        self.conn.request("GET", "/languages?api-version=3.0", headers=self.headers)
+        res = self.conn.getresponse()
+        data = json.loads(res.read())
+        self.lang = data["translation"]
+
+    def translate(self, text: str, to: list[str] = ['en']) -> dict:
+        payload = "[\r{\r\"Text\": \"" + text + "\"\r}\r]"
+        toStr = ""
+        for i in range(len(to)):
+            lang = to[i]
+            toStr += "to%5B" + str(i) + "%5D=" + lang + "&"
+        body = "/translate?" + toStr + "%3CREQUIRED%3E&api-version=3.0&profanityAction=NoAction&textType=plain"
+        
+        self.conn.request("POST", 
+                          body, 
+                          payload.encode(), 
+                          self.headers)
+
+        res = self.conn.getresponse()
+        data = json.loads(res.read())
+        srcLang = data[0]["detectedLanguage"]["language"]
+        result = data[0]["translations"]
+        return {
+            "srcLang": srcLang,
+            "result": result
+        }
+
+    def getNameOfLangFromCode(self, code: str) -> str:
+        return self.lang[code]["name"]
+
+trans = Translator()
+result = trans.translate("你好，你今天吃了什么？", ['en', 'de', 'it'])
+print("The source language is " + trans.getNameOfLangFromCode(result["srcLang"]))
+for r in result["result"]:
+    print(trans.getNameOfLangFromCode(r["to"]) + ": " + r["text"])
