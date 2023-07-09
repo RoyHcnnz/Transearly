62344e3a92ea6c2d68a37421bea85dd3efa1a556
RoyHcnnz <rhuangcnnz@gmail.com>
2023-07-08 19:42:47 -0700

    add context menu command

--- Transearly.py
+++ Transearly.py
@@ -1,3 +1,4 @@
+import functools
 import discord
 from discord.ext import commands
 from TranslateCore import Translator
@@ -5,7 +6,7 @@ from TranslateCore import Translator
 import Secrets
 
 intents = discord.Intents.default()
-#intents.message_content = True
+intents.message_content = True
 
 Transearly = commands.Bot("!", intents=intents)
 translator = Translator()
@@ -32,5 +33,27 @@ async def translate(interaction: discord.Interaction, text: str, targetlang: str
         replyEmbed.add_field(name=translator.getNameOfLangFromCode(r["to"]), value=r["text"], inline=False)
         # reply += "\n" + translator.getNameOfLangFromCode(r["to"]) + ": " + r["text"]
     await interaction.response.send_message(embed=replyEmbed, ephemeral=True)
+
+async def translateInMenu(interaction: discord.Interaction, message: discord.Message):
+    text = message.content
+    to = ["en", "zh-Hans"]
+    result = translator.translate(text, to)
+    replyEmbed = discord.Embed(title="Translate Result", description=text)
+    replyEmbed.add_field(
+        name="Source Language", 
+        value=translator.getNameOfLangFromCode(result["srcLang"]) + " [" + result["srcLang"] + "]", 
+        inline=False
+    )
+    # reply = "The source language is " + translator.getNameOfLangFromCode(result["srcLang"])
+    for r in result["result"]:
+        replyEmbed.add_field(name=translator.getNameOfLangFromCode(r["to"]), value=r["text"], inline=False)
+        # reply += "\n" + translator.getNameOfLangFromCode(r["to"]) + ": " + r["text"]
+    await interaction.response.send_message(embed=replyEmbed, ephemeral=True)
+
+translate_menu = discord.app_commands.ContextMenu(
+    name='translate',
+    callback=translateInMenu,
+)
+Transearly.tree.add_command(translate_menu)
 
 Transearly.run(Secrets.DISCORD_APP_TOKEN)
